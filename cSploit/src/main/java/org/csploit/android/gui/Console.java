c99e7a9c59799fa3415a281d48983c69899ceb1d
dorindorin97 <grecudorin97@gmail.com>
2025-12-07 17:07:10 +0000

    fix: Fix unescaped HTML quotes in Console.java
    Fixed color attribute quotes in fromHtml() calls that were
    breaking the Java string literals.

--- cSploit/src/main/java/org/csploit/android/gui/Console.java
+++ cSploit/src/main/java/org/csploit/android/gui/Console.java
@@ -57,7 +57,7 @@ public class Console extends AppCompatActivity {
                 Console.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mOutput.append(HtmlCompat.fromHtml("\n<font color="green">Enter Command:&gt;</font>  "));
+                        mOutput.append(HtmlCompat.fromHtml("\n<font color=\"green\">Enter Command:&gt;</font>  "));
                         mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
                         mScrollView.scrollBy(0, -mInput.getLineHeight());
                         mInput.setText("");
@@ -101,12 +101,12 @@ public class Console extends AppCompatActivity {
 
         mReceiver = new ConsoleReceiver();
         mSession = (ShellSession) System.getCurrentSession();
-        mOutput.append(HtmlCompat.fromHtml("<font color="green">Enter Command:&gt;</font>\n\n"));
+        mOutput.append(HtmlCompat.fromHtml("<font color=\"green\">Enter Command:&gt;</font>\n\n"));
         mInput.setImeActionLabel(getResources().getString(R.string.run), KeyEvent.KEYCODE_ENTER);
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mOutput.append(HtmlCompat.fromHtml("<font color="red">" + mInput.getText().toString() + "</font>\n"));
+                mOutput.append(HtmlCompat.fromHtml("<font color=\"red\">" + mInput.getText().toString() + "</font>\n"));
                 mSession.addCommand(mInput.getText().toString(), mReceiver);
             }
         });
@@ -114,7 +114,7 @@ public class Console extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT) {
-                    mOutput.append(HtmlCompat.fromHtml("<font color="red">" + mInput.getText().toString() + "</font>\n"));
+                    mOutput.append(HtmlCompat.fromHtml("<font color=\"red\">" + mInput.getText().toString() + "</font>\n"));
                     mSession.addCommand(mInput.getText().toString(), mReceiver);
                     return true;
                 }
@@ -126,7 +126,7 @@ public class Console extends AppCompatActivity {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN
                         && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    mOutput.append(HtmlCompat.fromHtml("<font color="red">" + mInput.getText().toString() + "</font>\n"));
+                    mOutput.append(HtmlCompat.fromHtml("<font color=\"red\">" + mInput.getText().toString() + "</font>\n"));
                     mSession.addCommand(mInput.getText().toString(), mReceiver);
                     return true;
                 }
